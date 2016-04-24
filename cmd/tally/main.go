002051ec4cb4e01d4e08c38eed1f11e66906de07
Vesa-Pekka Palmu <vpalmu@depili.fi>
2016-04-24 15:35:07 +0300

    Initial work on the relay control

--- cmd/tally/main.go
+++ cmd/tally/main.go
@@ -1,34 +1,61 @@
 package main
 
 import (
-    "github.com/qmsk/e2/client"
-    "github.com/qmsk/e2/discovery"
-    "github.com/jessevdk/go-flags"
-    "log"
-    "github.com/qmsk/e2/tally"
+	"e2/tally"
+	"fmt"
+	"github.com/jessevdk/go-flags"
+	"github.com/qmsk/e2/client"
+	"github.com/qmsk/e2/discovery"
+	"github.com/wujiang/embd"
+	_ "github.com/wujiang/embd/host/rpi" // This loads the RPi drive
+	"log"
 )
 
-var options = struct{
-    DiscoveryOptions    discovery.Options       `group:"E2 Discovery"`
-    ClientOptions       client.Options          `group:"E2 XML"`
-    TallyOptions        tally.Options           `group:"Tally"`
+var options = struct {
+	DiscoveryOptions discovery.Options `group:"E2 Discovery"`
+	ClientOptions    client.Options    `group:"E2 XML"`
+	TallyOptions     tally.Options     `group:"Tally"`
 }{}
 
 var parser = flags.NewParser(&options, flags.Default)
+var relays [8]embd.DigitalPin
 
 func main() {
-    if _, err := parser.Parse(); err != nil {
-        log.Fatalf("%v\n", err)
-    }
+	// Initialize GPIO
+	fmt.Printf("Initializing GPIO\n")
+	pins := [8]int{21, 20, 16, 12, 26, 19, 13, 6}
+	for i, pin := range pins {
+		fmt.Printf("Relay %d pin %d: ", i+1, pin)
+		relay, err := embd.NewDigitalPin(pins[i])
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf(" open")
+		defer relay.Close()
 
-    tally, err := options.TallyOptions.Tally(options.ClientOptions, options.DiscoveryOptions)
-    if err != nil {
-        log.Fatalf("Tally: %v\n", err)
-    }
+		if err := relay.SetDirection(embd.Out); err != nil {
+			panic(err)
+		}
+		fmt.Printf(" output")
+		if err := relay.Write(embd.Low); err != nil {
+			panic(err)
+		}
+		fmt.Printf(" low\n")
+		relays[i] = relay
+	}
 
-    if err := tally.Run(); err != nil {
-        log.Fatalf("Tally.Run: %v\n", err)
-    } else {
-        log.Printf("Exit")
-    }
+	if _, err := parser.Parse(); err != nil {
+		log.Fatalf("%v\n", err)
+	}
+
+	tally, err := options.TallyOptions.Tally(options.ClientOptions, options.DiscoveryOptions)
+	if err != nil {
+		log.Fatalf("Tally: %v\n", err)
+	}
+
+	if err := tally.Run(); err != nil {
+		log.Fatalf("Tally.Run: %v\n", err)
+	} else {
+		log.Printf("Exit")
+	}
 }
--- tally/tally.go
+++ tally/tally.go
@@ -1,110 +1,135 @@
 package tally
 
 import (
-    "github.com/qmsk/e2/client"
-    "github.com/qmsk/e2/discovery"
-    "fmt"
-    "log"
+	"fmt"
+	"github.com/qmsk/e2/client"
+	"github.com/qmsk/e2/discovery"
+	//	"github.com/wujiang/embd"
+	//	_ "github.com/wujiang/embd/host/rpi" // This loads the RPi drive
+	"log"
 )
 
 type Options struct {
-    clientOptions       client.Options
-    discoveryOptions    discovery.Options
+	clientOptions    client.Options
+	discoveryOptions discovery.Options
 }
 
 func (options Options) Tally(clientOptions client.Options, discoveryOptions discovery.Options) (*Tally, error) {
-    options.clientOptions = clientOptions
-    options.discoveryOptions = discoveryOptions
+	options.clientOptions = clientOptions
+	options.discoveryOptions = discoveryOptions
 
-    var tally Tally
+	var tally Tally
 
-    return &tally, tally.start(options)
+	return &tally, tally.start(options)
 }
 
 // Concurrent tally support for multiple sources and destinations
 type Tally struct {
-    options         Options
+	options Options
 
-    discovery       *discovery.Discovery
-    discoveryChan   chan discovery.Packet
+	discovery     *discovery.Discovery
+	discoveryChan chan discovery.Packet
 
-    /* run() state */
-    // active systems
-    sources         map[string]Source
+	/* run() state */
+	// active systems
+	sources map[string]Source
 
-    // updates to sources
-    sourceChan      chan Source
+	// updates to sources
+	sourceChan chan Source
 }
 
 func (tally *Tally) start(options Options) error {
-    tally.options = options
-    tally.sources = make(map[string]Source)
-    tally.sourceChan = make(chan Source)
+	tally.options = options
+	tally.sources = make(map[string]Source)
+	tally.sourceChan = make(chan Source)
 
-    if discovery, err := options.discoveryOptions.Discovery(); err != nil {
-        return fmt.Errorf("discovery:DiscoveryOptions.Discovery: %v", err)
-    } else {
-        tally.discovery = discovery
-        tally.discoveryChan = discovery.Run()
-    }
+	if discovery, err := options.discoveryOptions.Discovery(); err != nil {
+		return fmt.Errorf("discovery:DiscoveryOptions.Discovery: %v", err)
+	} else {
+		tally.discovery = discovery
+		tally.discoveryChan = discovery.Run()
+	}
 
-    return nil
+	return nil
 }
 
 // mainloop, owns Tally state
 func (tally *Tally) Run() error {
-    for {
-        select {
-        case discoveryPacket := <-tally.discoveryChan:
-            if clientOptions, err := tally.options.clientOptions.DiscoverOptions(discoveryPacket); err != nil {
-                log.Printf("Tally: invalid discovery client options: %v\n", err)
-            } else if _, exists := tally.sources[clientOptions.String()]; exists {
-                // already known
-            } else if source, err := newSource(tally, clientOptions); err != nil {
-                log.Printf("Tally: unable to connect to discovered system: %v\n", err)
-            } else {
-                log.Printf("Tally: connected to new source: %v\n", source)
+	for {
+		select {
+		case discoveryPacket := <-tally.discoveryChan:
+			if clientOptions, err := tally.options.clientOptions.DiscoverOptions(discoveryPacket); err != nil {
+				log.Printf("Tally: invalid discovery client options: %v\n", err)
+			} else if _, exists := tally.sources[clientOptions.String()]; exists {
+				// already known
+			} else if source, err := newSource(tally, clientOptions); err != nil {
+				log.Printf("Tally: unable to connect to discovered system: %v\n", err)
+			} else {
+				log.Printf("Tally: connected to new source: %v\n", source)
 
-                tally.sources[clientOptions.String()] = source
-            }
+				tally.sources[clientOptions.String()] = source
+			}
 
-        case source := <-tally.sourceChan:
-            if err := source.err; err != nil {
-                log.Printf("Tally: Source %v Error: %v\n", source, err)
+		case source := <-tally.sourceChan:
+			if err := source.err; err != nil {
+				log.Printf("Tally: Source %v Error: %v\n", source, err)
 
-                delete(tally.sources, source.String())
-            } else {
-                log.Printf("Tally: Source %v: Update\n", source)
+				delete(tally.sources, source.String())
+			} else {
+				log.Printf("Tally: Source %v: Update\n", source)
 
-                tally.sources[source.String()] = source
-            }
+				tally.sources[source.String()] = source
+			}
 
-            if err := tally.update(); err != nil {
-                return fmt.Errorf("Tally.update: %v\n", err)
-            }
-        }
-    }
+			if err := tally.update(); err != nil {
+				return fmt.Errorf("Tally.update: %v\n", err)
+			}
+		}
+	}
 }
 
 // Compute new output state from sources
 func (tally *Tally) update() error {
-    var state = State{
-        Inputs: make(map[Input]ID),
-        Tally:  make(map[ID]Status),
-    }
+	var state = State{
+		Inputs: make(map[Input]ID),
+		Tally:  make(map[ID]Status),
+	}
 
-    for _, source := range tally.sources {
-        if err := state.updateSystem(source.system, source.String()); err != nil {
-            return err
-        }
-    }
+	for _, source := range tally.sources {
+		if err := state.updateSystem(source.system, source.String()); err != nil {
+			return err
+		}
+	}
 
-    if err := state.update(); err != nil {
-        return err
-    }
+	if err := state.update(); err != nil {
+		return err
+	}
 
-    log.Printf("Tally.update: state:\n")
-    state.Print()
+	log.Printf("Tally.update: state:\n")
+	state.Print()
+	log.Printf("Set relays\n")
+	relay_states := [8]bool{false, false, false, false, false, false, false, false}
+	for i, status := range state.Tally {
+		if i < 9 && i > 0 {
+			relay_states[i-1] = status.Program
+		}
+	}
 
-    return nil
+	for i, enable := range relay_states {
+		fmt.Printf("relay %d: ", i+1)
+		if enable {
+			//			if err := relays[i].Write(embd.High); err != nil {
+			//				panic(err)
+			//			}
+			fmt.Printf("ON, ")
+		} else {
+			//			if err := relays[i].Write(embd.Low); err != nil {
+			//				panic(err)
+			//			}
+			fmt.Printf("off, ")
+		}
+	}
+	fmt.Printf("\n")
+
+	return nil
 }
